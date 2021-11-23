8831c0d0d471d4ff679e876dbe1bd104093ce4fa
Kier Durrani <kierdurrani@hotmail.com>
2021-11-23 18:41:38 +0000

    before messing with move logic

--- legalMove.js
+++ legalMove.js
@@ -1,129 +1,162 @@
-var piece = getPiece(coord);
-var CandidateLegalMoves = [];
+var LegalMoves = {};
+LegalMoves["a1"] = {gamestate : 'sugma', extendedState : {
+		"isWhitesTurn": true,
+		"whiteACastleable": true}};
 
-function transCoords(coord, x, y){
-	var newRank = coord[0].charCodeAt(0) - 96 + y; 
-	var newCol  = Number(coord[1]) + x;
 
-	if ( newCol < 1 ||  newCol > 8 ||  newRank < 1 || newRank > 8){
-		return null; // off the board
-	}else{
-		 return(String.fromCharCode(newRank + 96) + newCol);
-	}
-}
+console.log(LegalMoves["a1"].extendedState);
 
-function getPiece(coordinate){
 
-	var columnState = gamestate.split(";")[coordinate[1] - 1]; 
+function getLegalMoves(coord){
 	
-	var cellContent = columnState[columnIndex];      
-	
-	console.log("At coordinate: " + coordinate + " there is a " + cellContent );
-	
-	return cellContent;
-}
-	
+		
+		var piece = getPiece(coord);
+		var CandidateLegalMoves = {};
+		
+		CandidateLegalMoves[newCoords] = {gamestate : calculateBoardState(calculateBoardState(gamestate, coord, '_'), newCoords, piece), extendedStateChange : {"EnpassanablePawn": false}, promotion: false};
 
-function amIInCheck(){
-	// TODO
-}
-
-function isEnemyPiece(){
-	// possible performance increase by replacing this with "isAlliedPiece" and changing logic
+		function isEnemyPiece(piece){
+			// possible performance increase by replacing this with "isAlliedPiece" and changing logic
+			if(piece === "_") {return false;}
+			
+			var isWhitePiece  = (piece == piece.toLowerCase());
+			return !(isWhitePiece === extendedState.isWhitesTurn);
+		}
 	
-	
-}
+		function transCoords(coord, y, x){
+			var newRank = coord[0].charCodeAt(0) - 96 + y; 
+			var newCol  = Number(coord[1]) + x;
 
-function findAllMovesInLine(x,y){
-	var iter = 1;
-	var newCoords = transCoords(coord, x * iter, y * iter);
-	while( newCoords ){
-		if(getPiece(newCoords) == '_'){
-			CandidateLegalMoves.push(newCoords);
-			x++;
-		}else{ 
-			if(isEnemyPiece(getPiece(coord))){
-				CandidateLegalMoves.push(newCoords);
-				break; // If can capture, cant carry on after.
+			if ( newCol < 1 ||  newCol > 8 ||  newRank < 1 || newRank > 8){
+				return null; // off the board
 			}else{
-				break; // Cannot take own piece
+				 return(String.fromCharCode(newRank + 96) + newCol);
+			}
+		}		
+
+		function amIInCheck(){
+			// TODO
+		}
+
+		
+
+		function findAllMovesInLine(x,y){
+			var iter = 1;
+			var newCoords = transCoords(coord, x * iter, y * iter);
+			while( newCoords ){
+				if(getPiece(newCoords) == '_'){
+					CandidateLegalMoves.push(newCoords);
+				}else{ 
+					if(isEnemyPiece(getPiece(newCoords))){
+						CandidateLegalMoves.push(newCoords);
+						break; // If can capture, cant carry on after.
+					}else{
+						break; // Cannot take own piece
+					}
+				}
+				iter++;
+				newCoords = transCoords(coord,x * iter, y * iter);
+				
 			}
 		}
-		newCoords = transCoords(coord,x * iter, y * iter);
-		iter++;
-	}
-}
 
+		if( piece == 'R' || piece == 'r' ){  
+			
+			findAllMovesInLine(0,1);
+			findAllMovesInLine(1,0);
+			findAllMovesInLine(0,-1);
+			findAllMovesInLine(-1,0);
 
+		}
+		if( piece == 'N' || piece == 'n' ){  
+			
+			var allCoords = [transCoords(coord, 1, 2), transCoords(coord, 1, -2), transCoords(coord, -1, 2), transCoords(coord, -1, -2),	
+				transCoords(coord, 2, 1), transCoords(coord, 2, -1), transCoords(coord, -2, 1), transCoords(coord, -2, -1) ];
+			
+			for(var possCoords of allCoords){
+				// Possible null value error here..
+				if( !possCoords ){ continue; }
+				if( (getPiece(possCoords) == '_') || isEnemyPiece(getPiece(possCoords)) ){
+					CandidateLegalMoves.push(possCoords);
+				}		
+			}
+		} 
+		if( piece == 'B' || piece == 'b' ){  
 
-
-
-if( piece == 'R' || piece == 'r' ){  
-	
-	findAllMovesInLine(0,1);
-	findAllMovesInLine(1,0);
-	findAllMovesInLine(0,-1);
-	findAllMovesInLine(-1,0);
-
-}
-if( piece == 'N' || piece == 'n' ){  
-	
-	var allCoords = [transCoords(coord, 1, 2), transCoords(coord, 1, -2), transCoords(coord, -1, 2), transCoords(coord, -1, -2),	
-		transCoords(coord, 2, 1), transCoords(coord, 2, -1), transCoords(coord, -2, 1), transCoords(coord, -2, -1) ];
-	
-	foreach(possCoords in allCoords){
-		// Possible null value error here..
-		if( (getPiece(newCoords) == '_') || isEnemyPiece(getPiece(newCoords)) ){
-			CandidateLegalMoves.push(possCoords);
-		}		
-	}
-} 
-if( piece == 'B' || piece == 'b' ){  
-
-	findAllMovesInLine(+1,+1);
-	findAllMovesInLine(+1,-1);
-	findAllMovesInLine(-1,+1);	
-	findAllMovesInLine(-1,-1);
-} 
-if( piece == 'K' || piece == 'k' ){  
-	var allCoords = [transCoords(coord, 1, 1), transCoords(coord, 1, 0), transCoords(coord, 1, -1), transCoords(coord, 0, 1),	
-		transCoords(coord, 0, -1), transCoords(coord, -1, -1), transCoords(coord, -1, 0), transCoords(coord, -1, 1) ];
+			findAllMovesInLine(+1,+1);
+			findAllMovesInLine(+1,-1);
+			findAllMovesInLine(-1,+1);	
+			findAllMovesInLine(-1,-1);
+		} 
+		if( piece == 'K' || piece == 'k' ){  
+			var allCoords = [transCoords(coord, 1, 1), transCoords(coord, 1, 0), transCoords(coord, 1, -1), transCoords(coord, 0, 1),	
+				transCoords(coord, 0, -1), transCoords(coord, -1, -1), transCoords(coord, -1, 0), transCoords(coord, -1, 1) ];
+				
+			for(var possCoords of allCoords){
+				// Possible null value error here..
+				if( !possCoords ){ continue; }
+				if( (getPiece(possCoords) == '_') || isEnemyPiece(getPiece(possCoords)) ){
+					CandidateLegalMoves.push(possCoords);
+				}		
+			}
+			// TODO: Castling rules
+			
+		} 
+		if( piece == 'Q' || piece == 'q' ){  
+			findAllMovesInLine(+1,+1);
+			findAllMovesInLine(+1,-1);
+			findAllMovesInLine(-1,+1);	
+			findAllMovesInLine(-1,-1);
+			findAllMovesInLine(0,1);
+			findAllMovesInLine(1,0);
+			findAllMovesInLine(0,-1);
+			findAllMovesInLine(-1,0);
+		} 
 		
-	foreach(possCoords in allCoords){
-		// Possible null value error here..
-		if( (getPiece(newCoords) == '_') || isEnemyPiece(getPiece(newCoords)) ){
-			CandidateLegalMoves.push(possCoords);
-		}		
-	}
-	// TODO: Castling rules
-	
-} 
-if( piece == 'Q' || piece == 'q' ){  
-	findAllMovesInLine(+1,+1);
-	findAllMovesInLine(+1,-1);
-	findAllMovesInLine(-1,+1);	
-	findAllMovesInLine(-1,-1);
-	findAllMovesInLine(0,1);
-	findAllMovesInLine(1,0);
-	findAllMovesInLine(0,-1);
-	findAllMovesInLine(-1,0);
-} 
-if( piece == 'P' ){  
-	// TODO
-}
-if( piece == 'p' ){ 
-	
-	
-	if(coord.match('.2'){
-		// starting square
-	}else{
-		
-	} 
- } 
- 
-if( piece == '_'){
-	throw "This should not be possible - trying to move an empty square"
-}
+		if( piece === 'p' || piece === 'P' ){ 
+			
+			var direction = 1;
+			if(piece === "P"){
+				direction = -1;
+			}
+			
+			var newCoords = transCoords(coord, 0, direction * 1);
+			
+			// Pawn can move forwards
+			if( newCoords && getPiece(newCoords) === "_" ){
+				CandidateLegalMoves.push(newCoords);
+				
+				// Can move 2 spaces on first move:
+				if((piece === 'p' && coord.match('.2')) || (piece === 'P' && coord.match('.7'))){
+					newCoords = transCoords(coord, 0, direction * 2);
+					if( newCoords && getPiece(newCoords) === "_" ){
+						CandidateLegalMoves.push(newCoords);
+					}	
+				}
 
-return CandidateLegalMoves;
-// TODO: Validate not in check
+			}			
+			
+			// Pawn can take diagonally
+			newCoords = transCoords(coord, 1, direction * 1);
+			if(  newCoords && isEnemyPiece(getPiece(newCoords)) ){
+				CandidateLegalMoves.push(newCoords);
+			}
+			newCoords = transCoords(coord, -1, direction * 1);
+			if(  newCoords && isEnemyPiece(getPiece(newCoords)) ){
+				CandidateLegalMoves.push(newCoords);
+			}
+			
+			// En-Passant.
+			
+			// Promotion rules
+			var promotionWindow = "<>"
+			
+		 } 
+		 
+		if( piece == '_'){
+			throw "This should not be possible - trying to move an empty square"
+		}
+		// TODO: Validate not in checks
+		return CandidateLegalMoves;
+			
+	}
